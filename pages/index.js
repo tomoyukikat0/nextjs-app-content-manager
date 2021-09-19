cda127804e2c8c4c7e8fcf7b36bedb943d87d1da
Tomoyuki Kato <tomoyukikato@TomoyukinoMacBook-Pro.local>
2021-09-19 23:36:45 +0900

    modify index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,29 @@
-export default function Home() {
+function CompA(){
   return (
-    <h1>Hello world</h1>
+    <>
+      <h1>CompA</h1>
+      <p>Hello CompA</p>
+      <CompB />
+    </>
   )
 }
+
+function CompB() {
+  return (
+    <>
+      <h1>CompB</h1>
+      <p>Hello CompB</p>
+    </>
+  )
+}
+
+function Home() {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <CompA />
+    </>
+  )
+}
+
+export default Home;
