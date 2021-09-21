96062082061b7a680d7c5b29d4fa58de070c9180
Tomoyuki Kato <tomoyukikato@TomoyukinoMacBook-Pro.local>
2021-09-22 00:08:43 +0900

    props

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
 
-function CompA(){
+function CompA(allProps){
+  console.log(allProps)
   return (
     <>
       <h1>CompA</h1>
       <p>Hello Comp A</p>
+      <div>My Prop1: {allProps.myProp1}</div>
+      <div>My Prop1: {allProps.myProp2}</div>
+      <div>My Prop1: {allProps.myProp3.toString()}</div>
+      <div>My Prop1: {<allProps.myProp4 />}</div>
     </>
   )
 }
@@ -27,7 +32,12 @@ function Home() {
       CurrentState: { myValue }
       <button onClick={() => setValue(myValue + 1)}>+</button>
       <button onClick={() => setValue(myValue - 1)}>-</button>
-      <CompA />
+      <CompA 
+        myProp1={myValue}
+        myProp2="My custom Value"
+        myProp3={true}
+        myProp4={() => <div>My new JSX!</div>}
+      />
     </>
   )
 }
