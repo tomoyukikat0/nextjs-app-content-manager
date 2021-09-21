c53270ce33ecd117540335cf3e90191a03665236
Tomoyuki Kato <tomoyukikato@TomoyukinoMacBook-Pro.local>
2021-09-22 00:21:01 +0900

    useEffect

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,74 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function CompA(allProps){
-  console.log(allProps)
+
+  useEffect(() => {
+    console.log("CompA useEffect!");
+  }, [allProps.myProp1])
+
   return (
     <>
       <h1>CompA</h1>
       <p>Hello Comp A</p>
       <div>My Prop1: {allProps.myProp1}</div>
-      <div>My Prop1: {allProps.myProp2}</div>
-      <div>My Prop1: {allProps.myProp3.toString()}</div>
-      <div>My Prop1: {<allProps.myProp4 />}</div>
+      <div>My Prop2: {allProps.myProp2}</div>
+      <div>My Prop3: {allProps.myProp3.toString()}</div>
+      <div>My Prop4: {<allProps.myProp4 />}</div>
     </>
   )
 }
 
 class CompC extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      myValue: 10
+    }
+  }
+
+  changeState(incrementor) {
+    this.setState({
+      myValue: incrementor
+    })
+  }
+
   render() {
+    const { myValue } = this.state;
+
     return (
-      <h1>Hello CompC</h1>
+      <>
+        <h1>Hello CompC</h1>
+        Current Value: <h1>{ myValue }</h1>
+        <button onClick={() => this.changeState(myValue+1)}>+</button>
+        <button onClick={() => this.changeState(myValue-1)}>-</button>
+      </>
     )
   }
 }
 
 function Home() {
   const [myValue, setValue] = useState(10);
+  const [myOtherValue, setOtherValue] = useState(100);
 
-  console.log("I am called initialy and when state is changed!");
+  useEffect(() => {
+    // console.log("Use Effect Called!");
+  }, [myValue, myOtherValue])
 
   return (
     <>
-      CurrentState: { myValue }
-      <button onClick={() => setValue(myValue + 1)}>+</button>
-      <button onClick={() => setValue(myValue - 1)}>-</button>
-      <CompA 
+      Current Value: <h1>{ myValue }</h1>
+      <button onClick={() => setValue(myValue+1)}>+</button>
+      <button onClick={() => setValue(myValue-1)}>-</button>
+      <hr></hr>
+      Other Value: <h1>{ myOtherValue }</h1>
+      <button onClick={() => setOtherValue(myOtherValue+1)}>+</button>
+      <button onClick={() => setOtherValue(myOtherValue-1)}>-</button>
+      <CompA
         myProp1={myValue}
-        myProp2="My custom Value"
+        myProp2="My Custom Value"
         myProp3={true}
-        myProp4={() => <div>My new JSX!</div>}
+        myProp4={() => <div>My NEW JSX!</div>}
       />
     </>
   )
