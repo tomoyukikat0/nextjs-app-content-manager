0ef5f0655f675159242b61dde319f36cdb2dbbdb
Tomoyuki Kato <tomoyukikato@TomoyukinoMacBook-Pro.local>
2021-09-24 22:16:03 +0900

    absolete path

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import Navbar from "../components/Navbar";
-import ResourceHighlight from "../components/ResourceHighlight";
-import Newsletter from "../components/Newsletter";
-import ResourceList from "../components/ResourceList";
-import Footer from "../components/Footer";
+import Navbar from "components/Navbar";
+import ResourceHighlight from "components/ResourceHighlight";
+import Newsletter from "components/Newsletter";
+import ResourceList from "components/ResourceList";
+import Footer from "components/Footer";
 
 function Home() {
   return (
