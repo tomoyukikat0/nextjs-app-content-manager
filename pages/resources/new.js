5c4452f871c9b387b867b2c4a34ab3de2af8f2cb
Tomoyuki Kato <tomoyukikato@TomoyukinoMacBook-Pro.local>
2021-09-25 16:30:43 +0900

    modified

---
+++ pages/resources/new.js
@@ -0,0 +1,14 @@
+import Layout from "../../components/Layout";
+
+const ResourceCreate = () => {
+
+  return (
+    <Layout>
+      <div>
+        I am resource create page
+      </div>
+    </Layout>
+  )
+}
+
+export default ResourceCreate;
