96082724f8b78326f5400a174a19166a2936b6ac
Tomoyuki Kato <tomoyukikato@TomoyukinoMacBook-Pro.local>
2021-10-01 16:53:44 +0900

    introduce second state

--- components/ActiveResource.js
+++ components/ActiveResource.js
@@ -2,9 +2,10 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import moment from "moment";
- 
+
 const ActiveResource = () => {
   const [resource, setResource] = useState({});
+  const [seconds, setSeconds] = useState();
 
   useEffect(() => {
     async function fetchResource() {
@@ -16,6 +17,7 @@ const ActiveResource = () => {
 
       if (updatedTimeToFinish >= 0) {
         resource.timeToFinish = updatedTimeToFinish;
+        setSeconds(updatedTimeToFinish)
       }
 
       setResource(resource);
@@ -24,12 +26,24 @@ const ActiveResource = () => {
     fetchResource();
   }, [])
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds(seconds - 1)
+    }, 1000)
+
+    if (seconds < 0) {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [seconds])
+
   return (
     <div className="active-resource">
       <h1 className="resource-name">{resource.title}</h1>
       <div className="time-wrapper">
         <h2 className="elapsed-time">
-          {resource.timeToFinish}
+          {seconds}
         </h2>
       </div>
       <Link href="/">
