41e7e5b216e29917082711df1bfab547ea9ae3ea
Tomoyuki Kato <tomoyukikato@TomoyukinoMacBook-Pro.local>
2021-10-01 16:30:00 +0900

    create active resource components

--- components/ActiveResource.js
+++ components/ActiveResource.js
@@ -1,9 +1,21 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const ActiveResource = () => {
+  const [resource, setResource] = useState({});
+
+  useEffect(() => {
+    async function fetchResource() {
+      const axiosRes = await axios.get("/api/activesource")
+      const resource = axiosRes.data;
+      setResource(resource)
+    }
+  })
+
   return (
     <div className="active-resource">
-      <h1 className="resource-name">My Active Resource</h1>
+      <h1 className="resource-name">{resource.title}</h1>
       <div className="time-wrapper">
         <h2 className="elapsed-time">
           1400
--- pages/resources/[id]/index.js
+++ pages/resources/[id]/index.js
@@ -1,7 +1,15 @@
 import Layout from "../../../components/Layout"
 import Link from "next/link";
+import axios from "axios";
 
 const ResourceDetail = ({resource}) => {
+
+  const activeResource = () => {
+    axios.patch("/api/resources", {...resource, status: "active"})
+      .then(_ => alert("Resource has been activated!"))
+      .catch(_ => alert("Cannot active the resource!"))
+  }
+
   return (
     <Layout>
       <section className="hero ">
@@ -14,11 +22,17 @@ const ResourceDetail = ({resource}) => {
                     <h2 className="subtitle is-4">{resource.createdAt}</h2>
                     <h1 className="title">{resource.title}</h1>
                     <p>{resource.description}</p>
+                    <p>Time to finish: {resource.timeToFinish}</p>
                     <Link href={`/resources/${resource.id}/edit`}>
                       <a className="button is-warning">
                         Update
                       </a>
                     </Link>
+                    <button
+                      onClick={activeResource}
+                      className="button is-success ml-1">
+                        Active
+                    </button>
                   </div>
                 </div>
               </div>
